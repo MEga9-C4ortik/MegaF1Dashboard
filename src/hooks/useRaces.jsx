ce3e956af49a49b18c5025348c08b8d722c56437
Mega <mergenovezov987@gmail.com>
2026-03-05 09:40:54 +0800

    deployment error fix: Ебанный Jolpi file с большой буквы написал сука

--- src/hooks/useRaces.jsx
+++ src/hooks/useRaces.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { fetchRaces } from '../services/jolpiApi';
+import { fetchRaces } from '../services/JolpiApi';
 
 function useRaces(year) {
     const [races, setRaces] = useState([]);
